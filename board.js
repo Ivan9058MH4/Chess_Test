78dbb67bfd2ac381ef4988fb8fb19f5903352127
IVAN <Ivan>
2022-06-09 21:44:33 +0300

    full update

--- board.js
+++ board.js
@@ -1,17 +1,14 @@
-function sayHi () {
-  alert('Please, click OK to start')
-}
-sayHi()
+//function sayHi () {
+  //alert('Please, click OK to start')
+//}
+//sayHi()
 let table = document.createElement("table")
 for (let x = 0; x < 8; x++) {
-let tr = document.createElement("tr")
+let row = document.createElement("tr")
 for (let y = 0; y < 8; y++) {
   let td = document.createElement("td")
-  td.width = 50
-  td.height = 50
-  td.bgColor = (x + y) % 2 ? "black" : "LightYellow"
-  tr.appendChild(td)
+  row.appendChild(td)
 }
-table.appendChild(tr)
+table.appendChild(row)
 }
 document.body.appendChild(table) 
