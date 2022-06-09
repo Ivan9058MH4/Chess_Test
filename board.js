3a0b277c869c88287b8f9b111cb83e4b268113a3
IVAN <Ivan>
2022-06-09 13:35:41 +0300

    Check to make update chessboard

--- board.js
+++ board.js
@@ -1,17 +1,17 @@
 function sayHi () {
-    alert('Привет, юный Карпов!!!')
+  alert('Hi man')
 }
 sayHi()
-var chess = document.createElement("chess")
-for (var x = 0; x < 8; x++) {
-  var tr = document.createElement("tr")
-  for (var y = 0; y < 8; y++) {
-    var td = document.createElement("td")
-    td.width = 50
-    td.height = 50
-    td.bgColor = (x + y) % 2 ? "black" : "blue"
-    tr.appendChild(td)
-  }
-  chess.appendChild(tr)
+let chess = document.createElement("chess")
+for (let x = 0; x < 8; x++) {
+let tr = document.createElement("tr")
+for (let y = 0; y < 8; y++) {
+  let td = document.createElement("td")
+  td.width = 50
+  td.height = 50
+  td.bgColor = (x + y) % 2 ? "black" : "blue"
+  tr.appendChild(td)
 }
-document.body.appendChild(chess)
+chess.appendChild(tr)
+}
+document.body.appendChild(chess) 
